0085f5e7f4602d60cbdd71b8ec98249153a5e4ea
Robin <lotosv2010@163.com>
2025-06-17 11:17:19 +0800

    feat(g-vite): 实现支持 style

--- example/src/App.vue
+++ example/src/App.vue
@@ -14,3 +14,26 @@ export default {
   }
 }
 </script>
+
+<style>
+body {
+  margin: 0;
+  min-height: 100vh;
+  width: 100vw;
+  background-color: beige;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+h1 {
+  color: brown;
+  margin: 0;
+}
+p {
+  color: darkorange;
+}
+</style>
--- plugins/vue.js
+++ plugins/vue.js
@@ -3,39 +3,132 @@ const {
   compileScript,
   rewriteDefault,
   compileTemplate,
+  compileStyleAsync,
 } = require("vue/compiler-sfc");
 const fs = require("fs");
+const path = require("path");
+const hash = require("hash-sum");
 
+// 缓存
 const descriptorCache = new Map();
 
+/**
+ * 插件
+ */
 function vue() {
+  let root;
   return {
-    name: "vue",
+    name: "vue", // 插件名称
+    // 配置
+    config(config) {
+      root = config.root;
+    },
+    // 加载
+    async load(id) {
+      // 获取文件名
+      const { filename, query } = parseVueRequest(id);
+      // 如果是vue文件
+      if (query.has("vue")) {
+        // 获取描述
+        const descriptor = await getDescriptor(filename, root);
+        // 如果是style
+        if (query.get("type") === "style") {
+          // 获取样式
+          let block = descriptor.styles[Number(query.get("index"))];
+          // 如果有样式
+          if (block) {
+            // 返回样式
+            return { code: block.content };
+          }
+        }
+      }
+    },
+    // 转换
     async transform(code, id) {
-      const { filename } = parseVueRequest(id);
+      // 获取文件名
+      const { filename, query } = parseVueRequest(id);
+      // 如果是vue文件
       if (filename.endsWith(".vue")) {
-        let result = await transformMain(code, filename);
-        return result;
+        // 如果是style
+        if (query.get("type") === "style") {
+          // 获取描述
+          const descriptor = await getDescriptor(filename, root);
+          // 转换样式
+          let result = await transformStyle(
+            code,
+            descriptor,
+            query.get("index")
+          );
+          return result;
+        } else { // 如果是js
+          // 转换js
+          let result = await transformMain(code, filename, root);
+          return result;
+        }
       }
       return null;
     },
   };
 }
 
-async function getDescriptor(filename) {
+/**
+ * 样式转换
+ */
+async function transformStyle(code, descriptor, index) {
+  const block = descriptor.styles[index];
+  //如果是CSS，其实翻译之后和翻译之前内容是一样的
+  const result = await compileStyleAsync({
+    filename: descriptor.filename,
+    source: code,
+    id: `data-v-${descriptor.id}`, //必须传递，不然报错
+    scoped: block.scoped,
+  });
+  let styleCode = result.code;
+  const injectCode =
+    `\nvar  style = document.createElement('style');` +
+    `\nstyle.innerHTML = ${JSON.stringify(styleCode)};` +
+    `\ndocument.head.appendChild(style);`;
+  return {
+    code: injectCode,
+  };
+}
+
+/**
+ * 获取描述
+ */
+async function getDescriptor(filename, root) {
+  // 缓存
   let descriptor = descriptorCache.get(filename);
+  // 如果缓存有，直接返回
   if (descriptor) return descriptor;
+  // 获取文件内容
   const content = await fs.promises.readFile(filename, "utf8");
+  // 解析
   const result = parse(content, { filename });
+  // 赋值
   descriptor = result.descriptor;
+  // 生成id
+  descriptor.id = hash(path.relative(root, filename));
+  // 设置缓存
   descriptorCache.set(filename, descriptor);
   return descriptor;
 }
-async function transformMain(source, filename) {
-  const descriptor = await getDescriptor(filename);
+
+/**
+ * 转换主文件
+ */
+async function transformMain(source, filename, root) {
+  // 获取描述
+  const descriptor = await getDescriptor(filename, root);
+  // 脚本代码
   const scriptCode = genScriptCode(descriptor, filename);
+  // 模板代码
   const templateCode = genTemplateCode(descriptor, filename);
+  // 样式代码
+  const stylesCode = genStyleCode(descriptor, filename);
+  // 拼接代码
   let resolvedCode = [
+    stylesCode,
     templateCode,
     scriptCode,
     `_sfc_main['render'] = render`,
@@ -44,21 +137,58 @@ async function transformMain(source, filename) {
   return { code: resolvedCode };
 }
 
+/**
+ * 生成样式代码
+ */
+function genStyleCode(descriptor, filename) {
+  let styleCode = "";
+  // 如果有样式
+  if (descriptor.styles.length) {
+    // 遍历样式
+    descriptor.styles.forEach((style, index) => {
+      // 生成请求
+      const query = `?vue&type=style&index=${index}&lang=css`;
+      // 优化请求
+      const styleRequest = (filename + query).replace(/\\/g, "/");
+      // 生成导入
+      styleCode += `\nimport ${JSON.stringify(styleRequest)}`;
+    });
+    return styleCode;
+  }
+}
+
+/**
+ * 生成脚本代码
+ */
 function genScriptCode(descriptor, id) {
   let scriptCode = "";
+  // 获取脚本
   let script = compileScript(descriptor, { id });
   if (!script.lang) {
+    // 获取脚本内容
     scriptCode = rewriteDefault(script.content, "_sfc_main");
   }
   return scriptCode;
 }
+
+/**
+ * 模板代码
+ */
 function genTemplateCode(descriptor, id) {
+  // 获取模板
   let content = descriptor.template.content;
+  // 编译模板
   const result = compileTemplate({ source: content, id });
   return result.code;
 }
+
+/**
+ * 解析请求
+ */
 function parseVueRequest(id) {
+  // 获取文件名
   const [filename, querystring = ""] = id.split("?");
+  // 创建一个 URLSearchParams 对象
   let query = new URLSearchParams(querystring);
   return {
     filename,
